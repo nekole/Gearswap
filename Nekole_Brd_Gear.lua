1de78060f8e729227951e15f2a57e70f4281b131
nekole <33404711+nekole@users.noreply.github.com>
2017-12-24 07:50:20 -0600

    Add files via upload

--- Nekole_Brd_Gear.lua
+++ Nekole_Brd_Gear.lua
@@ -4,9 +4,6 @@ function user_setup()
     state.CastingMode:options('Normal', 'Resistant')
     state.IdleMode:options('Normal', 'PDT')
 
-	brd_daggers = S{'Izhiikoh', 'Atoyac', 'Aphotic Kukri'}
-	pick_tp_weapon()
-	
 	gear.mabstaff = {name="Serenity",augments={"DMG:+18", "CHR+1", "Mag.Atk.Bns+27"}}
 	gear.maccstaff = {name="Serenity",augments={"DMG:+12", "MND+2", "Mag. Acc.+22"}}
 	
@@ -16,7 +13,7 @@ function user_setup()
     info.ExtraSongs = 1
 	
 	-- Set this to false if you don't want to use custom timers.
-    state.UseCustomTimers = M(true, 'Use Custom Timers')
+    state.UseCustomTimers = M(false, 'Use Custom Timers')
 	
 	-- Additional local binds
     send_command('bind ^` gs c cycle ExtraSongsMode')
@@ -24,6 +21,8 @@ function user_setup()
 	send_command('bind @` gs c cycle MagicBurst')
 	send_command('bind @f10 gs c cycle RecoverMode')
 	send_command('bind @f8 gs c toggle AutoNukeMode')
+	send_command('bind !q gs c weapons NukeWeapons;gs c update')
+	send_command('bind ^q gs c weapons Swords;gs c update')
 
 	select_default_macro_book()
 end
@@ -34,24 +33,28 @@ function init_gear_sets()
 	-- Start defining the sets
 	--------------------------------------
 	
+	sets.Weapons = {main="Aeneas",sub="Kali"}
+	sets.Swords = {main="Vampirism",sub="Vampirism"}
+	sets.NukeWeapons = {main="Malevolence",sub="Malevolence"}
+	
 	-- Precast Sets
 
 	-- Fast cast sets for spells
-	sets.precast.FC = {main="Marin Staff +1",sub="Clerisy Strap +1",ammo="Impatiens",
-		head="Nahtirah Hat",neck="Voltsurge Torque",ear2="Loquac. Earring",
-		body="Vanir Cotehardie",hands="Gendewitha Gages +1",ring1="Prolix Ring",ring2="Veneficium Ring",
-		back="Swith Cape +1",waist="Witful Belt",legs="Artsieq Hose",feet="Chelona Boots +1"}
+	sets.precast.FC = {main=gear.grioavolr_fc_staff,sub="Clerisy Strap +1",ammo="Impatiens",
+		head="Nahtirah Hat",neck="Voltsurge Torque",ear1="Enchntr. Earring +1",ear2="Loquacious Earring",
+		body="Inyanga Jubbah +2",hands="Leyline Gloves",ring1="Kishar Ring",ring2="Lebeche Ring",
+		back="Intarabus's Cape",waist="Witful Belt",legs="Aya. Cosciales +2",feet="Gende. Galosh. +1"}
 
-	sets.precast.FC.Cure = set_combine(sets.precast.FC, {main="Serenity",sub="Curatio Grip",legs="Nabu's Shalwar",waist="Acerbic Sash +1"})
+	sets.precast.FC.Cure = set_combine(sets.precast.FC, {feet="Vanya Clogs"})
 
 	sets.precast.FC['Enhancing Magic'] = set_combine(sets.precast.FC, {waist="Siegel Sash"})
 	
 	sets.precast.Stoneskin = set_combine(sets.precast.FC['Enhancing Magic'], {})
 
-	sets.precast.FC.BardSong = {main="Felibre's Dague",sub="Genmei Shield",range="Linos",
-		head="Aoidos' Calot +2",neck="Voltsurge Torque",ear1="Aoidos' Earring",ear2="Loquac. Earring",
-		body="Sha'ir Manteel",hands="Gendewitha Gages +1",ring1="Prolix Ring",ring2="Veneficium Ring",
-		back="Swith Cape +1",waist="Witful Belt",legs="Gendewitha Spats +1",feet="Battlecast Gaiters"}
+	sets.precast.FC.BardSong = {main=gear.grioavolr_fc_staff,sub="Clerisy Strap +1",range="Linos",ammo=empty,
+		head="Nahtirah Hat",neck="Voltsurge Torque",ear1="Enchntr. Earring +1",ear2="Loquacious Earring",
+		body="Inyanga Jubbah +2",hands="Leyline Gloves",ring1="Kishar Ring",ring2="Lebeche Ring",
+		back="Intarabus's Cape",waist="Witful Belt",legs="Aya. Cosciales +2",feet="Telchine Pigaches"}
 
 	sets.precast.FC.Daurdabla = set_combine(sets.precast.FC.BardSong, {range=info.ExtraSongInstrument})
 		
@@ -63,16 +66,14 @@ function init_gear_sets()
 	sets.precast.JA['Soul Voice'] = {legs="Bihu Cannions +1"}
 
 	-- Waltz set (chr and vit)
-	sets.precast.Waltz = {range="Gjallarhorn",
-		head="Nahtirah Hat",
-		legs="Gendewitha Spats +1",feet="Gende. Galosh. +1"}
+	sets.precast.Waltz = {}
 
 	-- Weaponskill sets
 	-- Default set for any weaponskill that isn't any more specifically defined
-	sets.precast.WS = {ammo="Ginsen",
-		head="Brioso Roundlet +1",neck="Asperity Necklace",ear1="Steelflash Earring",ear2="Bladeborn Earring",
-		body="Bihu Jstcorps +1",hands="Brioso Cuffs +1",ring1="Rajas Ring",ring2="Patricius Ring",
-		back="Atheling Mantle",waist="Fotia Belt",legs="Bihu Cannions +1",feet="Gende. Galosh. +1"}
+	sets.precast.WS = {ammo="Hasty Pinion +1",
+		head="Aya. Zucchetto +1",neck="Caro Necklace",ear1="Moonshade Earring",ear2="Ishvara Earring",
+		body="Ayanmo Corazza +2",hands="Aya. Manopolas +1",ring1="Ilabrat Ring",ring2="Ramuh Ring +1",
+		back="Ground. Mantle +1",waist="Grunfeld Rope",legs="Aya. Cosciales +2",feet="Aya. Gambieras +1"}
 
 	-- Specific weaponskill sets.  Uses the base set if an appropriate WSMod version isn't found.
 
@@ -80,30 +81,30 @@ function init_gear_sets()
 	-- Midcast Sets
 
 	-- General set for recast times.
-	sets.midcast.FastRecast = {main=gear.maccstaff,sub="Clerisy Strap +1",
-		head="Nahtirah Hat",neck="Voltsurge Torque",ear2="Loquac. Earring",
-		body="Bihu Jstcorps +1",hands="Gendewitha Gages +1",ring1="Prolix Ring",ring2="Veneficium Ring",
-		back="Swith Cape +1",waist="Witful Belt",legs="Artsieq Hose",feet="Bihu Slippers +1"}
+	sets.midcast.FastRecast = {main=gear.grioavolr_fc_staff,sub="Clerisy Strap +1",ammo="Hasty Pinion",
+		head="Nahtirah Hat",neck="Voltsurge Torque",ear1="Enchntr. Earring +1",ear2="Loquacious Earring",
+		body="Inyanga Jubbah +2",hands="Leyline Gloves",ring1="Kishar Ring",ring2="Lebeche Ring",
+		back="Intarabus's Cape",waist="Witful Belt",legs="Aya. Cosciales +2",feet="Gende. Galosh. +1"}
 
 	-- Gear to enhance certain classes of songs.  No instruments added here since Gjallarhorn is being used.
 	sets.midcast.Ballad = {legs="Aoidos' Rhing. +2"}
-	sets.midcast.Lullaby = {hands="Brioso Cuffs +1"}
-	sets.midcast.Madrigal = {head="Aoidos' Calot +2"}
-	sets.midcast.Paeon = {head="Brioso Roundlet +1"}
-	sets.midcast.March = {hands="Aoidos' Manchettes +2"}
-	sets.midcast.Minuet = {body="Aoidos' Hongreline +2"}
+	sets.midcast.Lullaby = {neck="Ainia Collar",hands="Brioso Cuffs +1"}
+	sets.midcast.Madrigal = {head="Fili Calot"}
+	sets.midcast.Paeon = {head="Brioso Roundlet"}
+	sets.midcast.March = {hands="Fili Manchettes"}
+	sets.midcast.Minuet = {body="Fili Hongreline"}
 	sets.midcast.Minne = {}
-	sets.midcast.Carol = {body="Emet Harness +1"}
-	sets.midcast["Sentinel's Scherzo"] = {feet="Aoidos' Cothrn. +2"}
+	sets.midcast.Carol = {}
+	sets.midcast["Sentinel's Scherzo"] = {feet="Fili Cothurnes"}
 	sets.midcast['Magic Finale'] = {}
 
-	sets.midcast.Mazurka = {}
+	sets.midcast.Mazurka = {range="Terpander"}
 	
 
 	-- For song buffs (duration and AF3 set bonus)
 	sets.midcast.SongEffect = {main="Kali",sub="Genmei Shield",range="Linos",
 		head="Aoidos' Calot +2",neck="Aoidos' Matinee",ear2="Loquacious Earring",
-		body="Aoidos' Hongreline +2",hands="Aoidos' Manchettes +2",ring1="Prolix Ring",
+		body="Aoidos' Hongreline +2",hands="Aoidos' Manchettes +2",ring1="Stikini Ring",ring2="Stikini Ring",
 		back="Swith Cape +1",waist="Witful Belt",legs="Mdk. Shalwar +1",feet="Brioso Slippers +1"}
 
 	-- For song defbuffs (duration primary, accuracy secondary)
@@ -121,7 +122,7 @@ function init_gear_sets()
 	-- Song-specific recast reduction
 	sets.midcast.SongRecast = {main=gear.maccstaff,sub="Clerisy Strap +1",
 		head="Nahtirah Hat",neck="Voltsurge Torque",ear2="Loquac. Earring",
-		body="Bihu Jstcorps +1",hands="Gendewitha Gages +1",ring1="Prolix Ring",ring2="Veneficium Ring",
+		body="Bihu Jstcorps +1",hands="Gendewitha Gages +1",ring1="Kishar Ring",ring2="Prolix Ring",
 		back="Harmony Cape",waist="Corvax Sash",legs="Aoidos' Rhingrave +2",feet="Bihu Slippers +1"}
 
 	-- Cast spell with normal gear, except using Daurdabla instead
@@ -133,11 +134,11 @@ function init_gear_sets()
 	-- Other general spells and classes.
 	sets.midcast.Cure = {main="Arka IV",sub='Achaq Grip',
 		head="Gendewitha Caubeen +1",neck="Erra Pendant",ear1="Gifted Earring",ear2="Etiolation Earring",
-		body="Gende. Bilaut +1",hands="Telchine Gloves",ring1="Haoma's Ring",ring2="Haoma's Ring",
+		body="Gende. Bilaut +1",hands="Telchine Gloves",ring1="Janniston Ring",ring2="Haoma's Ring",
 		back="Tempered Cape +1",waist=gear.ElementalObi,legs="Gyve Trousers",feet="Gende. Galosh. +1"}
 		
 	sets.Self_Healing = {neck="Phalaina Locket",hands="Buremte Gloves",ring2="Kunaji Ring",waist="Gishdubar Sash"}
-	sets.Cure_Recieved = {neck="Phalaina Locket",hands="Buremte Gloves",ring2="Kunaji Ring",waist="Gishdubar Sash"}
+	sets.Cure_Received = {neck="Phalaina Locket",hands="Buremte Gloves",ring2="Kunaji Ring",waist="Gishdubar Sash"}
 	sets.Self_Refresh = {back="Grapevine Cape",waist="Gishdubar Sash"}
 		
 	sets.midcast['Enhancing Magic'] = {main="Serenity",sub="Fulcio Grip",ammo="Hasty Pinion +1",
@@ -178,19 +179,19 @@ function init_gear_sets()
 	sets.resting = {legs="Assid. Pants +1",feet="Chelona Boots +1"}
 	
 	-- Swap to these on Moonshade using WS if at 3000 TP
-	sets.precast.MaxTP = {ear1="Jupiter's Pearl",ear2="Kuwunga Earring"}
-	sets.precast.AccMaxTP = {ear1="Zennaroi Earring",ear2="Steelflash Earring"}	
+	sets.MaxTP = {ear1="Jupiter's Pearl",ear2="Kuwunga Earring"}
+	sets.AccMaxTP = {ear1="Zennaroi Earring",ear2="Steelflash Earring"}	
 	
 	-- Idle sets (default idle set not needed since the other three are defined, but leaving for testing purposes)
 	sets.idle = {main="Terra's Staff", sub="Achaq Grip",range="Gjallarhorn",
 		head=empty,neck="Loricate Torque +1",ear1="Moonshade Earring",ear2="Ethereal Earring",
 		body="Respite Cloak",hands=gear.chironic_refresh_hands,ring1="Defending Ring",ring2="Woltaris Ring",
-		back="Umbra Cape",waist="Flume Belt",legs="Assiduity Pants",feet="Serpentes Sabots"}
+		back="Umbra Cape",waist="Flume Belt",legs="Assiduity Pants",feet="Bihu Slippers +1"}
 
 	sets.idle.Weak = {main="Terra's Staff", sub="Oneiros Grip",range="Gjallarhorn",
 		head=empty,neck="Loricate Torque +1",ear1="Moonshade Earring",ear2="Ethereal Earring",
 		body="Respite Cloak",hands=gear.chironic_refresh_hands,ring1="Defending Ring",ring2="Dark Ring",
-		back="Umbra Cape",waist="Flume Belt",legs="Assiduity Pants",feet="Serpentes Sabots"}
+		back="Umbra Cape",waist="Flume Belt",legs="Assiduity Pants",feet="Bihu Slippers +1"}
 	
 	sets.idle.PDT = {main="Terra's Staff", sub="Oneiros Grip",range="Gjallarhorn",
 		head="Bihu Roundlet +1",neck="Loricate Torque +1",ear1="Moonshade Earring",ear2="Ethereal Earring",
@@ -202,12 +203,12 @@ function init_gear_sets()
 	sets.defense.PDT = {main="Terra's Staff", sub="Umbra Strap",range="Gjallarhorn",
 		head="Bihu Roundlet +1",neck="Loricate Torque +1",ear1="Moonshade Earring",ear2="Ethereal Earring",
 		body="Bihu Jstcorps +1",hands="Bihu Cuffs +1",ring1="Defending Ring",ring2="Dark Ring",
-		back="Moonbeam Cape",waist="Flume Belt",legs="Bihu Cannions +1",feet="Bihu Slippers +1"}
+		back="Moonlight Cape",waist="Flume Belt",legs="Bihu Cannions +1",feet="Bihu Slippers +1"}
 
 	sets.defense.MDT = {main="Terra's Staff", sub="Umbra Strap",range="Gjallarhorn",
 		head="Bihu Roundlet +1",neck="Loricate Torque +1",ear1="Moonshade Earring",ear2="Ethereal Earring",
 		body="Bihu Jstcorps +1",hands="Bihu Cuffs +1",ring1="Defending Ring",ring2="Dark Ring",
-		back="Moonbeam Cape",waist="Flume Belt",legs="Bihu Cannions +1",feet="Bihu Slippers +1"}
+		back="Moonlight Cape",waist="Flume Belt",legs="Bihu Cannions +1",feet="Bihu Slippers +1"}
 
 	sets.Kiting = {feet="Aoidos' Cothurnes +2"}
 
@@ -222,26 +223,18 @@ function init_gear_sets()
 	-- If you create a set with both offense and defense modes, the offense mode should be first.
 	-- EG: sets.engaged.Dagger.Accuracy.Evasion
 	
-	-- Basic set for if no TP weapon is defined.
 	sets.engaged = {ammo="Ginsen",
-		head="Brioso Roundlet +1",neck="Asperity Necklace",ear1="Steelflash Earring",ear2="Bladeborn Earring",
-		body="Bihu Jstcorps +1",hands="Brioso Cuffs +1",ring1="Rajas Ring",ring2="Patricius Ring",
+		head="Brioso Roundlet +1",neck="Asperity Necklace",ear1="Brutal Earring",ear2="Telos Earring",
+		body="Bihu Jstcorps +1",hands="Brioso Cuffs +1",ring1="Petrov Ring",ring2="Patricius Ring",
 		back="Atheling Mantle",waist="Ninurta's Sash",legs="Bihu Cannions +1",feet="Gende. Galosh. +1"}
 
-	-- Sets with weapons defined.
-	sets.engaged.Dagger = {ammo="Ginsen",
-		head="Brioso Roundlet +1",neck="Asperity Necklace",ear1="Steelflash Earring",ear2="Bladeborn Earring",
-		body="Bihu Jstcorps +1",hands="Brioso Cuffs +1",ring1="Rajas Ring",ring2="Patricius Ring",
-		back="Atheling Mantle",waist="Ninurta's Sash",legs="Bihu Cannions +1",feet="Gende. Galosh. +1"}
-
-	-- Set if dual-wielding
-	sets.engaged.DualWield = {ammo="Ginsen",
-		head="Brioso Roundlet +1",neck="Asperity Necklace",ear1="Steelflash Earring",ear2="Bladeborn Earring",
-		body="Bihu Jstcorps +1",hands="Brioso Cuffs +1",ring1="Rajas Ring",ring2="Patricius Ring",
+	sets.engaged.DW = {ammo="Ginsen",
+		head="Brioso Roundlet +1",neck="Asperity Necklace",ear1="Suppanomimi",ear2="Telos Earring",
+		body="Bihu Jstcorps +1",hands="Brioso Cuffs +1",ring1="Petrov Ring",ring2="Patricius Ring",
 		back="Atheling Mantle",waist="Ninurta's Sash",legs="Bihu Cannions +1",feet="Gende. Galosh. +1"}
 end
 
 -- Select default macro book on initial load or subjob change.
 function select_default_macro_book()
-	set_macro_page(3, 5)
+	set_macro_page(10, 10)
 end
